9e48c0ca41af5c3f7f8819d83cfdd886c7393c0a
fede183 <fede.183@hotmail.com>
2021-12-26 16:38:23 -0300

    se agregaron test para la resolución de formulas

--- calculator_gui_rust/src/main.rs
+++ calculator_gui_rust/src/main.rs
@@ -1,4 +1,5 @@
 use eframe::{egui, epi};
+mod math;
 
 struct MyApp {
     formula: String,
@@ -28,7 +29,7 @@ impl epi::App for MyApp {
                 ui.text_edit_singleline(formula);
             });
 
-            let rows = [["1", "2", "3", "+"], ["4", "5", "6", "-"], ["7", "8", "9", "*"], ["", "0", "", "/"]];
+            let rows = [["1", "2", "3", "+"], ["4", "5", "6", "-"], ["7", "8", "9", "*"], ["(", "0", ")", "/"]];
 
             for row in rows {
                 ui.horizontal(|ui| {
@@ -42,7 +43,7 @@ impl epi::App for MyApp {
 
             ui.horizontal(|ui| {
                 if ui.button("=").clicked() {
-                    let resolved_formula : Result<f32, f32> = solve_math(&*formula);
+                    let resolved_formula : Result<f32, f32> = math::solve_math(&*formula);
                     if resolved_formula.is_ok() {
 
                         *formula = resolved_formula.unwrap().to_string();
@@ -61,66 +62,6 @@ impl epi::App for MyApp {
     }
 }
 
-fn is_numeric(possible_number : &String) -> bool {
-    let test = possible_number.parse::<f64>();
-    match test {
-        Ok(_ok) => true,
-        Err(_e) => false, 
-    }  
-}
-
-fn have_operators(formula : &String) -> bool {
-    let operators = String::from("+-*/");
-    
-    for operator in operators.chars() {
-        if formula.contains(operator) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-fn solve_math(formula : &String) -> Result<f32, f32> {
-    
-    if !have_operators(formula) && !is_numeric(formula) {
-        return Err(0.0);
-    }
-
-    if is_numeric(formula) {
-        let number_formula : f32 = formula.parse().unwrap();
-        return Ok(number_formula);
-    }
-
-    let possible_operators = String::from("*/+-");
-
-       
-    for operator in possible_operators.chars() {
-        if formula.contains(operator) {
-            let formula_parts : Vec<&str> = formula.split(operator).collect();
-
-            let solved_formula_1 : Result<f32, f32> = solve_math(&String::from(formula_parts[0]));
-            let solved_formula_2 : Result<f32, f32> = solve_math(&String::from(formula_parts[1]));
-
-            if solved_formula_1.is_err() || solved_formula_2.is_err()  {
-                return Err(0.0);
-            }
-
-            let solved_formula_1 : f32 = solved_formula_1.unwrap();
-            let solved_formula_2 : f32 = solved_formula_2.unwrap();
-    
-            return match operator {
-                '+' => Ok(solved_formula_1 + solved_formula_2),
-                '-' => Ok(solved_formula_1 - solved_formula_2),
-                '*' => Ok(solved_formula_1 * solved_formula_2),
-                '/' => Ok(solved_formula_1 / solved_formula_2),
-                _ => Err(0.0),
-            };
-        }
-    }
-
-    return Err(0.0);
-}
 
 fn main() {
     let options = eframe::NativeOptions::default();
