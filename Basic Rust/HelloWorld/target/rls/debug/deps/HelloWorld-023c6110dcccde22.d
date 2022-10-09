055f4ae88c8a2211929239589f12f13a6ad0a031
fede183 <fede.183@hotmail.com>
2022-10-09 11:56:24 -0300

    simple projects

--- Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-023c6110dcccde22.d
+++
@@ -1,7 +0,0 @@
-/home/nimchimsky/Documents/Projects/Excercises-in-various-languages/Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-023c6110dcccde22.rmeta: src/main.rs src/prints.rs src/vars.rs
-
-/home/nimchimsky/Documents/Projects/Excercises-in-various-languages/Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-023c6110dcccde22.d: src/main.rs src/prints.rs src/vars.rs
-
-src/main.rs:
-src/prints.rs:
-src/vars.rs:
--- Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-c45f42deeef57572.d
+++
@@ -1,7 +0,0 @@
-/home/nimchimsky/Documents/Projects/Excercises-in-various-languages/Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-c45f42deeef57572.rmeta: src/main.rs src/prints.rs src/vars.rs
-
-/home/nimchimsky/Documents/Projects/Excercises-in-various-languages/Basic Rust/HelloWorld/target/rls/debug/deps/HelloWorld-c45f42deeef57572.d: src/main.rs src/prints.rs src/vars.rs
-
-src/main.rs:
-src/prints.rs:
-src/vars.rs:
